f3e29a2ee63b868b04d08731460c0dd08663843c
R729a <rudrakshsahu001@gmail.com>
2026-05-23 22:33:33 +0530

    Doubly linked list.c

---
+++ Linked_list/Doubly linked list.c
@@ -0,0 +1,292 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Structure of Node
+struct Node
+{
+    int data;
+    struct Node *next;
+    struct Node *prev;
+};
+
+// Function to create a new node
+struct Node *createNode(int data)
+{
+    struct Node *newNode = (struct Node *)malloc(sizeof(struct Node));
+    newNode->data = data;
+	newNode->prev = NULL;
+    newNode->next = NULL;
+    return newNode;
+}
+
+// Traversal function
+void traverseForward(struct Node *head)
+{
+    if (head == NULL)
+    {
+        printf("Linked List is empty\n");
+    }
+
+    struct Node *temp = head;
+    printf("Linked List Forward : ");
+    while (temp != NULL)
+    {
+        printf("%d <-> ", temp->data);
+        temp = temp->next;
+    }
+    printf("NULL\n");
+}
+
+void traverseBackward(struct Node *head)
+{
+    if (head == NULL)
+    {
+        printf("Linked List is empty\n");
+    }
+
+    struct Node *temp = head;
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
+    }
+    printf("Linked List Backward: ");
+    while (temp != NULL)
+    {
+        printf("%d <-> ", temp->data);
+        temp = temp->prev;
+    }
+    printf("NULL\n");
+}
+
+// Insert at beginning
+struct Node *insertAtBeginning(struct Node *head, int data)
+{
+    struct Node *newNode = createNode(data);
+    if (head != NULL)
+    {
+        head->prev = newNode;
+        newNode->next = head;
+    }
+    return newNode;
+}
+
+// Insert at end
+struct Node *insertAtEnd(struct Node *head, int data)
+{
+    struct Node *newNode = createNode(data);
+
+    if (head == NULL)
+    {
+        return newNode;
+    }
+
+    struct Node *temp = head;
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
+    }
+    temp->next = newNode;
+    newNode->prev = temp;
+    return head;
+}
+
+// Insert after given position
+struct Node *insertAtMiddle(struct Node *head, int data, int pos)
+{
+    struct Node *newNode = createNode(data);
+    struct Node *temp = head;
+    int i = 0;
+
+    while (i < pos - 1 && temp != NULL)
+    {
+        temp = temp->next;
+        i++;
+    }
+
+    if (temp == NULL)
+    {
+        printf("Invalid position!\n");
+        return head;
+    }
+
+    newNode->next = temp->next;
+    newNode->prev = temp;
+
+    if (temp->next != NULL)
+        temp->next->prev = newNode;
+
+    temp->next = newNode;
+
+    return head;
+}
+
+// Delete begining node
+struct Node *deleteBeginningNode(struct Node *head)
+{
+    if (head == NULL)
+    {
+        printf("List is empty!\n");
+        return head;
+    }
+    struct Node *temp = head;
+    int deletedValue = temp->data;
+    printf("%d element deleted\n", deletedValue);
+    head = head->next;
+    if (head != NULL)
+        head->prev = NULL;
+    free(temp);
+    return head;
+}
+
+//Delete Last node
+struct Node *deleteEndNode(struct Node *head)
+{
+    struct Node *temp = head;
+    struct Node *q = temp->next;
+
+    if (head == NULL)
+    {
+        printf("List is empty!\n");
+        return head;
+    }
+
+    if (head->next == NULL)
+    {
+        printf("%d deleted\n", head->data);
+        free(head);
+        return NULL;
+    }
+
+    while (temp->next != NULL){
+
+        temp = temp->next;
+}
+printf("%d element deleted\n", temp->data);
+
+temp->prev->next = NULL;
+free(temp);
+return head;
+}
+
+//CASE DELETE AT GIVEN INDEX
+struct Node *deleteinbetween(struct Node *head, int index)
+{
+    if (head == NULL)
+    {
+        printf("List empty\n");
+        return head;
+    }
+
+    struct Node *temp = head;
+    int i = 0;
+
+    while (i < index && temp != NULL)
+    {
+        temp = temp->next;
+        i++;
+    }
+
+    if (temp == NULL)
+    {
+        printf("Invalid index\n");
+        return head;
+    }
+
+    printf("%d deleted\n", temp->data);
+
+    if (temp->prev != NULL)
+        temp->prev->next = temp->next;
+
+    if (temp->next != NULL)
+        temp->next->prev = temp->prev;
+
+    if (temp == head)
+        head = temp->next;
+
+    free(temp);
+    return head;
+}
+
+// Main Menu Program
+int main()
+{
+    struct Node *head = NULL;
+    int choice, data, pos, index;
+
+    while (1)
+    {
+        printf("\n--- MENU ---\n");
+        printf("1. Insert at Beginning\n");
+        printf("2. Insert at Middle\n");
+        printf("3. Insert at End\n");
+        printf("4. Traverse Forward\n");
+        printf("5. Traverse Backward\n");
+        printf("6.Delete first node\n");
+        printf("7.Delete Last Node\n");
+        printf("8.Delete by index\n");
+        printf("9. Exit\n");
+
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+        switch (choice)
+        {
+        case 1:
+            printf("Enter data: ");
+            scanf("%d", &data);
+            head = insertAtBeginning(head, data);
+            break;
+
+        case 2:
+            printf("Enter data: ");
+            scanf("%d", &data);
+            printf("Enter position (after which to insert): ");
+            scanf("%d", &pos);
+            head = insertAtMiddle(head, data, pos);
+            break;
+
+        case 3:
+            printf("Enter data: ");
+            scanf("%d", &data);
+            head = insertAtEnd(head, data);
+            break;
+
+        case 4:
+            traverseForward(head);
+            break;
+
+        case 5:
+            traverseBackward(head);
+            break;
+
+        case 6:
+            head = deleteBeginningNode(head);
+
+            break;
+
+        case 7:
+            head = deleteEndNode(head);
+
+            break;
+
+        case 8:
+            printf("Enter the index:");
+            if (scanf("%d", &index) == 1 && index > 0)
+            {
+                head = deleteinbetween(head, index);
+            }
+            else
+            {
+                printf("Invalid input! Index %d allowed nahi hai.\n", index);
+            }
+            break;
+
+        case 9:
+            printf("Exiting...\n");
+            exit(0);
+
+        default:
+            printf("Invalid choice!\n");
+        }
+    }
+}
